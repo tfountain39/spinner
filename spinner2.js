c10f3efa88a255425ae58429d30b531ed24177ca
Todd <todd.fountain39@gmail.com>
2023-09-26 21:26:17 -0500

    Upload of refactored spinner

--- spinner2.js
+++ spinner2.js
@@ -1,3 +1,5 @@
+// Sources: GPT3, MDN
+
 const progressIcon = [`\r| `, `\r/  `, `\r- `, `\r\\  `, `\r|  `, `\r/  `, `\r|  `, `\r/  `, `\r/  `, `\r-  `, `\r-  `, `\r\\  `]
 // For each character in the progressIcon array, write to stdoubt at a scheduled interval
 chars.forEach((progressIcon, i) => setTimeout(() => process.stdout.write(progressIcon), i * 200));
